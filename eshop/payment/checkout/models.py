997467ca3a56b1fd1dbe019a15a5c6978f9b6e0a
meshkat <emiliyafttpx@gmail.com>
2022-05-20 12:10:23 +0430

    add all files to repo

---
+++ eshop/payment/checkout/models.py
@@ -0,0 +1,5 @@
+from oscar.apps.checkout.models import * 
+from django.db import models
+from oscar.core.loading import is_model_registered
+from oscar.apps.partner.abstract_models import AbstractPartner
+
---
+++ eshop/payment/checkout/views.py
@@ -0,0 +1,42 @@
+import logging
+from django.urls import reverse
+from azbankgateways import bankfactories, models as bank_models, default_settings as settings
+from azbankgateways.exceptions import AZBankGatewaysException
+from django.shortcuts import render
+from oscar.apps.checkout.views import PaymentDetailsView as CorePaymentDetailsView
+
+class PaymentDetailsView(CorePaymentDetailsView):
+   def submit(self, user, basket, shipping_address, shipping_method,
+               shipping_charge, billing_address, order_total,
+               payment_kwargs=None, order_kwargs=None, surcharges=None):
+      return self.handle_order_placement(
+                 user, basket, shipping_address, shipping_method,
+                shipping_charge, billing_address, order_total, surcharges=surcharges, **order_kwargs)
+
+order_total = PaymentDetailsView()
+
+def go_to_gateway_view(request):
+    amount = 1000
+
+    # user_mobile_number = '+989112221234' 
+
+    factory = bankfactories.BankFactory()
+    try:
+        bank = factory.auto_create() # or factory.create(bank_models.BankType.ZARINPAL) or set identifier
+        bank.set_request(request)
+        bank.set_amount(amount)
+        # یو آر ال بازگشت به نرم افزار برای ادامه فرآیند
+        bank.set_client_callback_url(reverse('callback-gateway'))
+        # bank.set_mobile_number(user_mobile_number)  # اختیاری
+    
+        # در صورت تمایل اتصال این رکورد به رکورد فاکتور یا هر چیزی که بعدا بتوانید ارتباط بین محصول یا خدمات را با این
+        # پرداخت برقرار کنید. 
+        bank_record = bank.ready()
+        
+        # هدایت کاربر به درگاه بانک
+        return bank.redirect_gateway()
+    except AZBankGatewaysException as e:
+        logging.critical(e)
+        # TODO: redirect to failed page.
+        raise e
+    return(request, 'templates/partials/basket_content.html')
